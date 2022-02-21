30c11418eb2f0a31724d290d34e8eed74de174f7
Anders <ahpyenson@gmail.com>
2022-02-21 17:47:02 -0600

    Removed (not working) color code.

--- Encrypt-Decrypt/encrypt-decrypt.py
+++ Encrypt-Decrypt/encrypt-decrypt.py
@@ -82,16 +82,6 @@ def getUserInput():
 
     }
     please_text = ""
-    class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKCYAN = '\033[96m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
 
     # Encrypt or decrypt.
     valid = False
